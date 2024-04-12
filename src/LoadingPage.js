2eaa67cf3409854bb12feeda0d5a03f1068189f6
Adrian <adrianvalverde45@gmail.com>
2024-04-11 22:46:38 -0400

    updated loading pages color, text, and font

--- src/LoadingPage.js
+++ src/LoadingPage.js
@@ -1,11 +1,14 @@
 import React from "react";
+import './LoadingPage.css'
 
 function LoadingPage() {
     return (
-        <div class="intro">
-            <h1 class="logo-header">
-                <span>Develop,</span><span>Innovate,</span><span>Empower.</span>
-            </h1>
+        <div className="intro">
+            <div className="text-container">
+                <span className="element">Develop,</span>
+                <span className="element">Innovate,</span>
+                <span className="element">Empower.</span>
+            </div>
         </div>
     )
 }
