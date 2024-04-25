c80dbfd3d5b360311ebccc2e50f02aaa7c5d99c6
Adrian <adrianvalverde45@gmail.com>
2024-04-25 13:47:11 -0400

    updated css on portfolio component

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -5,7 +5,15 @@ function Portfolio() {
     return (
         <div>
             <div className="background">
-                <h1>Welcome</h1>
+                <div className="fadeIn">
+                    <p>Adrian</p>
+                    <p>The</p>
+                    <p>Develop</p>
+                    <div className="flexbox">
+                        <h4 className="Headline-2">Full Stack Developer</h4>
+                        <h1 className="Headline">I develop inspiring and captivating websites.</h1>
+                    </div>
+                </div>
             </div>
         </div>
     )
