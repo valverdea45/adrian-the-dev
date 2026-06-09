7dd0e6afcc1c372df27e0f5c65d48978298eb9b3
Adrian <adrianvalverde45@gmail.com>
2026-06-09 16:50:57 -0400

    updated intro css size width ad height

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -11,7 +11,7 @@ function Portfolio() {
                     <p>Develop</p>
                     <div className="flexbox">
                         <h4 className="Headline-2">Full Stack Developer</h4>
-                        <h1 className="Headline">I develop inspiring and captivating websites.</h1>
+                        <h1 className="Headline">Developing inspiring and captivating websites.</h1>
                     </div>
                 </div>
             </div>
