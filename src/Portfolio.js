37e0e0e90cb2db03a19b9311432a5be1e572544d
Adrian <adrianvalverde45@gmail.com>
2026-06-09 17:52:50 -0400

    changed background color to name to be rainbow purple

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -6,12 +6,11 @@ function Portfolio() {
         <div>
             <div className="background">
                 <div className="fadeIn">
-                    <p>Adrian</p>
-                    <p>The</p>
-                    <p>Develop</p>
                     <div className="flexbox">
+                        <h1 className="name-headline">Adrian Valverde</h1>
+                        
                         <h4 className="Headline-2">Full Stack Developer</h4>
-                        <h1 className="Headline">Developing inspiring and captivating websites.</h1>
+                        <h4 className="Headline">Developing inspiring and captivating websites.</h4>
                     </div>
                 </div>
             </div>
