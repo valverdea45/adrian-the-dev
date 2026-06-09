abf33425d140ad5af461d3b44b625bd9f19536ce
Adrian <adrianvalverde45@gmail.com>
2026-06-09 18:21:17 -0400

    tryig new background

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -8,7 +8,6 @@ function Portfolio() {
                 <div className="fadeIn">
                     <div className="flexbox">
                         <h1 className="name-headline">Adrian Valverde</h1>
-                        
                         <h1 className="Headline-2">Full Stack Developer</h1>
                         <h1 className="Headline">Developing inspiring and captivating websites.</h1>
                     </div>
